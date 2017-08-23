f15d5a626f962025f2015564609c8aad977aeb8f
Danilo Lima <danilo.moura.lima@gmail.com>
2017-08-23 15:04:44 +0200

    Getting stadium information

--- run.rb
+++ run.rb
@@ -15,11 +15,6 @@ module Chupinhator
       @client = Mongo::Client.new(['127.0.0.1:27017'], database: 'chupinhator', connect: :direct)
       countries = retrieve_countries
       stadiums = retrieve_stadiums(countries)
-
-      puts countries.count
-      puts stadiums.count
-      save_countries(countries)
-      save_stadiums(stadiums)
     end
 
     private
@@ -44,31 +39,66 @@ module Chupinhator
         country[:url]     = node.css('a').map{|link| link['href']}.first
         country[:total]   = node.css('a strong').text
         node.css('a span').remove
-        country[:country] = node.css('a').text
-        #puts country
+        country[:name] = node.css('a').text
+
+        puts "Saving country: #{country[:name]}"
+        save_country(country)
         countries << country
       end
 
       countries
     end
 
-    # STEP 2 - VISIT THE COUNTRY URL
+    # STEP 2 - VISIT THE COUNTRY URL AND GET STADIUM URL
     def retrieve_stadiums(countries)
       stadiums = []
 
       countries.each do |country|
         result = Typhoeus.get(country[:url], followlocation: true).body
-        doc = Nokogiri::HTML(result)
-        stadium = {}
-        doc.css('.table-stadiums tr').each do |node|
-          next if node.css('a').empty?
-          stadium[:url] = node.css('a').map{|link| link['href']}.first
-          row = node.css('td').text.gsub!("\t", "").split("\n").reject!(&:empty?)
-          stadium[:name] = row[0]
-          stadium[:city] = row[1]
-          stadium[:teams] = row[2]
-          stadium[:capacity] = row[3].split(" ").join.to_i
-          #puts stadium
+        country =  Nokogiri::HTML(result)
+        country.css('.table-stadiums').xpath("//tr[position() > 1]").css('a').map{|link| link['href']}.each do |url|
+          puts url
+          result = Typhoeus.get(url, followlocation: true).body
+
+          doc = Nokogiri::HTML(result)
+          #doc.xpath('//text()').find_all {|t| t.to_s.strip == ''}.map(&:remove)
+          stadium = {}
+
+          stadium[:capacity] = {
+            total: doc.css('.stadium-info .capacity > td').text,
+            additional: doc.css('.stadium-info .capacity-comp > td').text
+          }
+          stadium[:country]      = doc.css('.stadium-info .flag-m').css('a').text
+
+          doc.css('.stadium-info tr').each do |row|
+            stadium[:city]              = row.css('td').text if row.css('th').children.text == "City"
+            stadium[:clubs]             = row.css('td').text if row.css('th').children.text == "Clubs"
+            stadium[:other_names]       = row.css('td').text if row.css('th').children.text == "Other names"
+            stadium[:inauguration]      = row.css('td').text if row.css('th').children.text == "Inauguration"
+            stadium[:construction]      = row.css('td').text if row.css('th').children.text == "Construction"
+            stadium[:cost]              = row.css('td').text if row.css('th').children.text == "Cost"
+            stadium[:design]            = row.css('td').text if row.css('th').children.text == "Design"
+            stadium[:contractor]        = row.css('td').text if row.css('th').children.text == "WContractor"
+            stadium[:address]           = row.css('td').text if row.css('th').children.text == "Address"
+            stadium[:cost]              = row.css('td').text if row.css('th').children.text == "Cost"
+            stadium[:record_attendance] = row.css('td').text if row.css('th').children.text == "Record attendance"
+          end
+
+          stadium[:name]         = doc.css('.stadium-description h1').children.to_html.gsub("Description: ", "")
+          stadium[:description]  = doc.css('.stadium-description p').children.to_html
+          stadium[:images]       = []
+
+          doc.css('.stadium-pictures li').children.each do |row|
+            next if row.is_a?(Nokogiri::XML::Text)
+            image = {}
+            image[:url] = row.css('a').attr('href').text
+            image[:alt] = row.css('img').attr('alt').text
+            image[:caption] = row.css('figcaption').text
+            stadium[:images] << image
+          end
+
+          puts "Saving stadium: #{stadium[:name]}"
+          save_stadium(stadium)
           stadiums << stadium
         end
       end
@@ -76,16 +106,12 @@ module Chupinhator
       stadiums
     end
 
-    def save_countries(countries)
-      countries.each do |country|
-        countries_collection.insert_one(country)
-      end
+    def save_country(country)
+      countries_collection.insert_one(country)
     end
 
-    def save_stadiums(stadiums)
-      stadiums.each do |stadium|
-        stadiums_collection.insert_one(stadium)
-      end
+    def save_stadium(stadium)
+      stadiums_collection.insert_one(stadium)
     end
   end
 end
